32e999c4b10ee21cbab3c4f908ffbd9086af3846
Student <example@example.com>
2020-03-09 10:51:06 -0700

    modify cors page

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://obscure-tor-14240.herokuapp.com/'
+    origins 'http://localhost:3001'
 
     resource '*',
       headers: :any,
