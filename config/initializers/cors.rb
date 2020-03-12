aaea36737332247098fd88720816e9008371ce8d
Student <example@example.com>
2020-03-12 13:11:49 -0700

    update CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://lit-sea-37901.herokuapp.com/'
+    origins 'https://localhost:3000/'
 
     resource '*',
       headers: :any,
