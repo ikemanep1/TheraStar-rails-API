e32abb84366de44c154288ec3614b293e69662a9
Isaac Esquivel-Pilloud <ike.esquivelpilloud@gmail.com>
2020-04-25 15:11:40 -0700

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://lit-sea-37901.herokuapp.com/'
+    origins 'https://lit-sea-37901.herokuapp.com/#/'
 
     resource '*',
       headers: :any,
